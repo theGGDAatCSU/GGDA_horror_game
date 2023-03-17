1189faccdddc84f05e4b128bbb9ff1520a125e5d
theGGDAatCSU <128095380+theGGDAatCSU@users.noreply.github.com>
2023-03-16 20:07:06 -0400

    Inventory, Interactor, and Interactables
    Inventory, Interactor, and Interactables have been started. Worked on the development of the UI functionality. Hotbar has been started.

---
+++ GGDA_horror_gameProject/Assets/Scripts/Interactable.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public bool collectible;
+    public bool door;
+    public GameObject objectPrefab;
+    public Sprite objectSprite;
+    
+    public void CollectObject()
+    {
+
+    }
+    public void InteractObject()
+    {
+
+    }
+}
